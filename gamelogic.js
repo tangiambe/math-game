b33bd1bc9c3d527cc2271a7443f3b58ba23af938
tangiambe <tafor@yahoo.com>
2024-04-19 09:43:10 -0400

    game logic in javascript

---
+++ gamelogic.js
@@ -0,0 +1,115 @@
+var playing = false;
+var score;
+var action;
+var timeleft;
+var correctAnswer;
+
+
+document.getElementById("startreset").onclick = function(){
+    
+    if(playing == true){
+
+        location.reload();
+
+    }else{
+        
+        playing = true;     
+        score = 0;
+        document.getElementById("scorevalue").innerHTML = score;
+     
+        
+        show("timeleft");
+        timeleft = 30;
+        document.getElementById("timeleft-value").innerHTML = timeleft;
+        
+        
+        hide("gameOver");
+        
+        document.getElementById("instruction").innerHTML = "Choose the correct answer";
+        document.getElementById("startreset").innerHTML = "Reset Game";
+        
+        startCountdown();
+        generateQA();
+    }
+    
+}
+
+
+for(i=1; i<5; i++){
+    document.getElementById("box"+i).onclick = function(){     
+    if(playing == true){
+        if(this.innerHTML == correctAnswer){
+            score++;
+            document.getElementById("scorevalue").innerHTML = score;
+            hide("wrong");
+            show("correct");
+            setTimeout(function(){
+                hide("correct");   
+            }, 1000);
+        
+            generateQA();
+        }else{
+            hide("correct");
+            show("wrong");
+            setTimeout(function(){
+                hide("wrong");   
+            }, 1000);
+        }
+    }
+}   
+}
+
+function startCountdown(){
+    action = setInterval(function(){
+        timeleft -= 1;
+        document.getElementById("timeleft-value").innerHTML = timeleft;
+        if(timeleft == 0){
+            stopCountdown();
+            show("gameOver");
+         document.getElementById("gameOver").innerHTML = "<p>Game over!</p><p>Your score is: " + score + "</p>";   
+            hide("timeleft");
+            hide("correct");
+            hide("wrong");
+            playing = false;
+            document.getElementById("startreset").innerHTML = "Start Game";
+        }
+    }, 1000);    
+}
+
+function stopCountdown(){
+    clearInterval(action);   
+}
+
+
+function hide(Id){
+    document.getElementById(Id).style.display = "none";   
+}
+
+
+function show(Id){
+    document.getElementById(Id).style.display = "block";   
+}
+
+
+function generateQA(){
+    var x = 1+ Math.round(9*Math.random());
+    var y = 1+ Math.round(9*Math.random());
+    correctAnswer = x*y;
+    document.getElementById("question").innerHTML = x + "x" + y;
+    var correctPosition = 1+ Math.round(3*Math.random());
+    document.getElementById("box"+correctPosition).innerHTML = correctAnswer;
+    
+    
+    var answers = [correctAnswer];
+    
+    for(i=1; i<5; i++){
+        if(i != correctPosition) {
+            var wrongAnswer;
+            do{
+                wrongAnswer = (1+ Math.round(9*Math.random()))*(1+ Math.round(9*Math.random()));
+            }while(answers.indexOf(wrongAnswer)>-1)
+            document.getElementById("box"+i).innerHTML = wrongAnswer;
+            answers.push(wrongAnswer);
+        }
+    }
+}
